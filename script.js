76db4822d3d24bc602fc694dcd46b6ce6c23d237
Pablo Ontiveros <pablacas@gmail.com>
2020-06-21 21:37:03 -0500

    implemented pseudo-code

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 var view = document.querySelector(".jumbotron");
+var highScore = document.querySelector("#scores");
+var quiz = document.querySelector("#quiz");
 var rules = ["THE RULES", "1)", "2)", "3)","4)"];
+var time = 75;
+
+
+
+//helper functions
+
+//clears the quiz window
+function clear(){
+
+}
+
+//starts the timer
+function timer(){
+
+
+    if (time === 0){
+        scoreWindow();
+    }
+}
+
+//keeps track of scores
+function score(){
+
+}
+
+//Makes score window
+function scoreWindow(){
+
+}
+
+//presents questions
+function questions(){
+
+}
+
 //fills the jumbotron with the quiz start screen
 function welcome(){
     // Creates the title.
@@ -20,5 +57,41 @@ function welcome(){
     start.setAttribute("class", "btn btn-success");
     view.appendChild(start);
 };
+
+
+
+
+// starts quizz
+view.addEventListener("click", function(event){
+event.preventDefault();
+if(event.target.matches("button")) {
+    console.log("Quiz Time")
+    //clear view
+    clear();
+    //start timer
+    timer();
+    //present question
+    questions();
+    //track score
+  }
+});
+
+//shows the leaderboard
+highScore.addEventListener("click", function(event){
+    event.preventDefault();
+    highScore.setAttribute("class", "nav-link active");
+    quiz.setAttribute("class", "nav-link");
+    clear();
+
+});
+
+//takes you to quiz start window 
+quiz.addEventListener("click", function(event){
+    event.preventDefault();
+    quiz.setAttribute("class", "nav-link active");
+    highScore.setAttribute("class", "nav-link");
+    clear();
+    welcome();
+});
 
 welcome();
